b9ce094e07e129fbb15903ef16cfbb6d15b90717
reddysai741 <140647039+reddysai741@users.noreply.github.com>
2023-08-02 14:25:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Project1{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        c=a+b;
+        System.out.println(c);
+    }
+}
